b224f52932e62e3eed778933eb723b021ac97d70
s3rious a.k.a Leonid Semenov <canismajor@kaschenka.ru>
2020-07-04 10:57:11 +0300

    Fetch posts in sync to await 'Too many requests error'

--- src/get-wall-posts-with-documents.ts
+++ src/get-wall-posts-with-documents.ts
@@ -1,6 +1,7 @@
 import {VKApi} from 'node-vk-sdk'
+import {WallWallpostFull} from "node-vk-sdk/distr/src/generated/Models";
 
-const getWallPostsWithDocuments = async (api: VKApi, group_id: number) => {
+const getWallPostsWithDocuments = async (api: VKApi, group_id: number): Promise<Array<WallWallpostFull>> => {
   const owner_id = group_id * -1
   const {count} = await api.wallGet({owner_id, count: 1, extended: false})
 
@@ -31,18 +32,32 @@ const getWallPostsWithDocuments = async (api: VKApi, group_id: number) => {
     return map
   })()
 
-  return (
-    await Promise.all(requestsMap.map(({offset, count}) =>
-      api.wallGet({owner_id, offset, count})
-    ))
-  )
-  .map(({items}) =>
-    items
-  )
-  .reduce((accum, items) =>
-    [...accum, ...items], []
-  )
-  .filter(item =>
+  const fetchAll = async () => {
+    let allData: WallWallpostFull[] = []
+
+    await requestsMap.reduce((acc, { offset, count }, index) =>
+      acc
+        .then(() => {
+          console.log(`Fetching ${index} out of ${requestsMap.length} posts chunks...`)
+
+          return api.wallGet({ owner_id, offset, count })
+        })
+        .then((data) => {
+          console.log('... fetched!')
+
+          allData = [...allData, ...data.items]
+
+          return
+        }),
+      Promise.resolve()
+    )
+
+    return allData
+  }
+
+  const data = await fetchAll()
+
+  return data.filter(item =>
     Boolean(item.attachments && item.attachments.find(attachment => attachment.type === 'doc'))
   )
 }
